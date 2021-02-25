a91ee92f23fc1b1db6b821c226982ee9db87de78
Wisser <wisserwendelin@gmail.com>
2021-02-25 20:53:09 +0100

    Initial commit

---
+++ src/main/kotlin/me/skylands/skypvp/SkyLands.kt
@@ -0,0 +1,11 @@
+package me.skylands.skypvp
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SkyLands : JavaPlugin() {
+
+    override fun onEnable() {
+        println("[SL] Hello World")
+    }
+
+}
