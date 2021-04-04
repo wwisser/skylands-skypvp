b2daec37adef7a3f9e877a73cadd3c289f257397
Wendelin Wisser <wisserwendelin@gmail.com>
2021-04-04 13:13:54 +0200

    Fix millis formatter

--- src/main/kotlin/me/skylands/skypvp/util/Formatter.kt
+++ src/main/kotlin/me/skylands/skypvp/util/Formatter.kt
@@ -8,31 +8,39 @@ object Formatter {
     /**
      * Converts the given amount of millis into a formatted String (days, hours, minutes, seconds)
      */
-    fun formatMillis(millis: Long): String {
-        var millisTmp = millis
-        val days = TimeUnit.MILLISECONDS.toDays(millisTmp)
-        millisTmp -= TimeUnit.DAYS.toMillis(days)
-        val hours = TimeUnit.MILLISECONDS.toHours(millisTmp)
-        millisTmp -= TimeUnit.HOURS.toMicros(hours)
-        val minutes = TimeUnit.MILLISECONDS.toMinutes(millisTmp)
-        millisTmp -= TimeUnit.MINUTES.toMillis(minutes)
-        val seconds = TimeUnit.MILLISECONDS.toSeconds(millisTmp)
+    fun formatMillis(mMillis: Long): String {
+        var millis = mMillis
+
+        val days = TimeUnit.MILLISECONDS.toDays(millis)
+        millis -= TimeUnit.DAYS.toMillis(days)
+        val hours = TimeUnit.MILLISECONDS.toHours(millis)
+        millis -= TimeUnit.HOURS.toMillis(hours)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(millis)
+        millis -= TimeUnit.MINUTES.toMillis(minutes)
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(millis)
+
         val results: MutableList<String> = LinkedList()
+
         if (days != 0L) {
             results.add(days.toString() + "d")
         }
+
         if (hours != 0L) {
             results.add(hours.toString() + "h")
         }
+
         if (minutes != 0L) {
             results.add(minutes.toString() + "m")
         }
+
         if (seconds != 0L) {
             results.add(seconds.toString() + "s")
         }
+
         return if (results.isEmpty()) {
             "1ms"
         } else java.lang.String.join(", ", results)
+
     }
 
 }
