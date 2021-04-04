a0bb019b4ebb0e9ec209be5449892afd309ede35
Wendelin Wisser <wisserwendelin@gmail.com>
2021-04-04 12:00:50 +0200

    fix allargs constructor

--- src/main/kotlin/me/skylands/skypvp/stats/context/impl/internal/KillToplistContext.java
+++ src/main/kotlin/me/skylands/skypvp/stats/context/impl/internal/KillToplistContext.java
@@ -1,18 +1,20 @@
 package me.skylands.skypvp.stats.context.impl.internal;
 
-import lombok.AllArgsConstructor;
 import me.skylands.skypvp.stats.context.ToplistContext;
 import me.skylands.skypvp.stats.label.StatsLabel;
 import me.skylands.skypvp.user.UserService;
 
 import java.util.Map;
 
-@AllArgsConstructor
 public class KillToplistContext implements ToplistContext {
 
     private static final StatsLabel STATS_LABEL = StatsLabel.KILLS;
 
     private UserService service;
+
+    public KillToplistContext(UserService service) {
+        this.service = service;
+    }
 
     @Override
     public StatsLabel getLabel() {
