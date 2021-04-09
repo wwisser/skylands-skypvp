beaa4c980b421636a699826f52f44c75535d2251
Wendelin Wisser <wisserwendelin@gmail.com>
2021-04-09 20:11:58 +0200

    Add welcome title, message & equip

--- src/main/kotlin/me/skylands/skypvp/listener/PlayerJoinListener.kt
+++ src/main/kotlin/me/skylands/skypvp/listener/PlayerJoinListener.kt
@@ -1,10 +1,14 @@
 package me.skylands.skypvp.listener
 
+import me.skylands.skypvp.Messages
 import me.skylands.skypvp.SkyLands
 import me.skylands.skypvp.task.TablistUpdateTask
 import me.skylands.skypvp.user.FileUserRepository
 import me.skylands.skypvp.user.UserRepository
+import me.skylands.skypvp.util.Title
+import org.bukkit.Bukkit
 import org.bukkit.Sound
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -15,7 +19,25 @@ class PlayerJoinListener : Listener {
     fun onPlayerJoin(event: PlayerJoinEvent) {
         val player = event.player
 
-        event.joinMessage = "§7[§6§l+§7] ${player.name}"
+        Title("§6§lSkyLands", "§7SkyPvP & SkyBlock", 0, 2, 1).send(player)
+
+        if (!player.hasPlayedBefore()) {
+            Bukkit.getOnlinePlayers()
+                .stream()
+                .filter { onlinePlayer: Player -> onlinePlayer !== player }
+                .forEach { onlinePlayer: Player ->
+                    onlinePlayer.sendMessage(
+                        Messages.PREFIX + "§7Willkommen auf §6§lSkyLands§7," +
+                                " §e" + player.name + "§7! "
+                                + "§7[§e#§l" + Bukkit.getOfflinePlayers().size + "§7]"
+                    )
+                }
+
+            PlayerRespawnListener.equipWithKit(player)
+        } else {
+            event.joinMessage = "§7[§6§l+§7] ${player.name}"
+        }
+
         player.playSound(player.location, Sound.LEVEL_UP, Float.MAX_VALUE, Float.MIN_VALUE)
         TablistUpdateTask.updateTablist(player)
 
@@ -28,6 +50,7 @@ class PlayerJoinListener : Listener {
         val loadUser = SkyLands.userService.loadUser(player)
         player.level = loadUser.level
         SkyLands.userService.getUser(player).lastSeen = System.currentTimeMillis()
+
     }
 
 }
--- src/main/kotlin/me/skylands/skypvp/listener/PlayerRespawnListener.kt
+++ src/main/kotlin/me/skylands/skypvp/listener/PlayerRespawnListener.kt
@@ -3,6 +3,7 @@ package me.skylands.skypvp.listener
 import me.skylands.skypvp.item.ItemBuilder
 import org.bukkit.Material
 import org.bukkit.Sound
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerRespawnEvent
@@ -13,39 +14,43 @@ class PlayerRespawnListener : Listener {
 
     companion object {
         const val AUTO_KIT_NAME = "§6§lAutokit"
+
+        private val AUTO_KIT_VOTE: List<ItemStack> = listOf(
+            ItemBuilder(Material.IRON_SWORD).name(AUTO_KIT_NAME).build(),
+            ItemBuilder(Material.COOKED_CHICKEN).amount(6).name(AUTO_KIT_NAME).build(),
+            ItemBuilder(Material.IRON_HELMET).name(AUTO_KIT_NAME).build(),
+            ItemBuilder(Material.IRON_CHESTPLATE).name(AUTO_KIT_NAME).build(),
+            ItemBuilder(Material.IRON_LEGGINGS).name(AUTO_KIT_NAME).build(),
+            ItemBuilder(Material.IRON_BOOTS).name(AUTO_KIT_NAME).build(),
+        )
+
+        private val AUTO_KIT_DEFAULT: List<ItemStack> = listOf(
+            ItemBuilder(Material.STONE_SWORD).name(AUTO_KIT_NAME).build(),
+            ItemBuilder(Material.COOKED_CHICKEN).amount(6).name(AUTO_KIT_NAME).build(),
+            ItemBuilder(Material.CHAINMAIL_HELMET).name(AUTO_KIT_NAME).build(),
+            ItemBuilder(Material.CHAINMAIL_CHESTPLATE).name(AUTO_KIT_NAME).build(),
+            ItemBuilder(Material.CHAINMAIL_LEGGINGS).name(AUTO_KIT_NAME).build(),
+            ItemBuilder(Material.CHAINMAIL_BOOTS).name(AUTO_KIT_NAME).build(),
+        )
+
+        fun equipWithKit(player: Player) {
+            val autoKit: List<ItemStack> = if (VotifierListener.hasVoteKit(player)) AUTO_KIT_VOTE else AUTO_KIT_DEFAULT
+
+            player.inventory.addItem(autoKit[0])
+            player.inventory.addItem(autoKit[1])
+            player.inventory.helmet = autoKit[2]
+            player.inventory.chestplate = autoKit[3]
+            player.inventory.leggings = autoKit[4]
+            player.inventory.boots = autoKit[5]
+            player.playSound(player.location, Sound.HORSE_SADDLE, 1f, 1f)
+        }
     }
 
-    private val AUTO_KIT_VOTE: List<ItemStack> = listOf(
-        ItemBuilder(Material.IRON_SWORD).name(AUTO_KIT_NAME).build(),
-        ItemBuilder(Material.COOKED_CHICKEN).amount(6).name(AUTO_KIT_NAME).build(),
-        ItemBuilder(Material.IRON_HELMET).name(AUTO_KIT_NAME).build(),
-        ItemBuilder(Material.IRON_CHESTPLATE).name(AUTO_KIT_NAME).build(),
-        ItemBuilder(Material.IRON_LEGGINGS).name(AUTO_KIT_NAME).build(),
-        ItemBuilder(Material.IRON_BOOTS).name(AUTO_KIT_NAME).build(),
-    )
 
-    private val AUTO_KIT_DEFAULT: List<ItemStack> = listOf(
-        ItemBuilder(Material.STONE_SWORD).name(AUTO_KIT_NAME).build(),
-        ItemBuilder(Material.COOKED_CHICKEN).amount(6).name(AUTO_KIT_NAME).build(),
-        ItemBuilder(Material.CHAINMAIL_HELMET).name(AUTO_KIT_NAME).build(),
-        ItemBuilder(Material.CHAINMAIL_CHESTPLATE).name(AUTO_KIT_NAME).build(),
-        ItemBuilder(Material.CHAINMAIL_LEGGINGS).name(AUTO_KIT_NAME).build(),
-        ItemBuilder(Material.CHAINMAIL_BOOTS).name(AUTO_KIT_NAME).build(),
-    )
 
     @EventHandler
     fun onPlayerRespawn(event: PlayerRespawnEvent) {
-        val player = event.player
-
-        val autoKit: List<ItemStack> = if (VotifierListener.hasVoteKit(player)) AUTO_KIT_VOTE else AUTO_KIT_DEFAULT
-
-        player.inventory.addItem(autoKit[0])
-        player.inventory.addItem(autoKit[1])
-        player.inventory.helmet = autoKit[2]
-        player.inventory.chestplate = autoKit[3]
-        player.inventory.leggings = autoKit[4]
-        player.inventory.boots = autoKit[5]
-        player.playSound(player.location, Sound.HORSE_SADDLE, 1f, 1f)
+        equipWithKit(event.player)
     }
 
 }
