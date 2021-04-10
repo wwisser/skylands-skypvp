2895897696fd0c6bcac4267ddb5c83db5233b2b6
Wendelin Wisser <wisserwendelin@gmail.com>
2021-04-10 12:15:27 +0200

    Delete old backups

--- src/main/scripts/backup.sh
+++ src/main/scripts/backup.sh
@@ -5,3 +5,4 @@ filename=backup-${now}.tar.gz
 ssh sl "tar -czf ${filename} /home/spigot/"
 scp sl:~/${filename} /var/backups/sl/${filename}
 ssh sl "rm ${filename}"
+find /var/backups/sl/* -mtime +5 -exec rm {} \; # delete backups older than 5 days
