dc3a02c884d23b8d3138323324df3adef3ecc604
Wendelin Wisser <wisserwendelin@gmail.com>
2021-04-13 13:38:23 +0200

    Increase backup time to 10d

--- src/main/scripts/backup.sh
+++ src/main/scripts/backup.sh
@@ -5,4 +5,4 @@ filename=backup-${now}.tar.gz
 ssh sl "tar -czf ${filename} /home/spigot/"
 scp sl:~/${filename} /var/backups/sl/${filename}
 ssh sl "rm ${filename}"
-find /var/backups/sl/* -mtime +5 -exec rm {} \; # delete backups older than 5 days
+find /var/backups/sl/* -mtime +10 -exec rm {} \; # delete backups older than 10 days
