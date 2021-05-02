026a64771ae68446f2183951868929b63188c05e
Wendelin Wisser <49454122+wwisser@users.noreply.github.com>
2021-05-02 21:48:25 +0200

    Update docker.sh

--- src/main/scripts/docker.sh
+++ src/main/scripts/docker.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker run -d -p 25565:25565 -p 8192:8192 -v /home/spigot:/minecraft --name spigot -e EULA=true -e SPIGOT_VER=1.8.8 -e MC_MAXMEM=3g -e MC_MINMEM=2g -e SPIGOT_AUTORESTART=yes nimmis/spigot
+
+# SkyPvP (spigot)
+docker run -d -p 25565:25565 -p 8192:8192 -v /home/spigot:/minecraft --name spigot -e EULA=true -e SPIGOT_VER=1.8.8 -e MC_MINMEM=1g -e SPIGOT_AUTORESTART=yes nimmis/spigot
+
+# Test (build)
+docker run -d -p 25566:25566 -v /home/build:/minecraft --name build -e EULA=true -e SPIGOT_VER=1.8.8 -e MC_MAXMEM=512m -e SPIGOT_AUTORESTART=yes nimmis/spigot
