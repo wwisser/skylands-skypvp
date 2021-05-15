8d9cd0b5f3a86ae5728f6540b5e97d5970716cdb
Nic <saurer.nic@gmail.com>
2021-05-15 18:16:47 +0200

    Update StatsLabel.java

--- src/main/kotlin/me/skylands/skypvp/stats/label/StatsLabel.java
+++ src/main/kotlin/me/skylands/skypvp/stats/label/StatsLabel.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 public enum StatsLabel {
 
     KILLS("Kills", "Kills", 2),
-    KILLSTREAK("Killstreak", "Killstreak", 3),
-    BLOODPOINTS("Blutpunkte", "Blutpunkte", 4),
+    //KILLSTREAK("Killstreak", "Killstreak", 3),
+    //BLOODPOINTS("Blutpunkte", "Blutpunkte", 4),
     DEATHS("Tode", "Tode", 5),
     VOTES("Votes", "Votes", 6),
     PLAYTIME("Spielzeit", "Stunden", 7),
